1f99b4da29fc8efcfcdd041d96589c05ef46c6f4
hasanarinc <hasan.arinc@appcent.mobi>
2023-12-06 17:12:54 +0300

    Complete firebase email password authentication structure

---
+++ app/src/main/java/com/appcent/android/firebasedemo/domain/util/extensions/FragmentExtensions.kt
@@ -0,0 +1,20 @@
+package com.appcent.android.firebasedemo.domain.util.extensions
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+/**
+ * Created by hasan.arinc on 4.12.2023.
+ */
+
+fun <T> Fragment.collectFlow(flow: Flow<T>, observer: (T) -> Unit) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collect { t -> observer(t) }
+        }
+    }
+}
